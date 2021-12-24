67e2af083e245e816190ffa9f337a309ba50b5b9
Luiz Henrique <7henrique18@gmail.com>
2021-12-23 23:47:16 -0300

    :tada: Init project

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Dashboard } from "./src/screens/Dashboard.tsx";
+
+export default function App() {
+  return <Dashboard />;
+}
