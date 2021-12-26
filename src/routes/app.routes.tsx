4e957aa57330233f0ab01b0c20fb3d0f7c41ebbc
Luiz Henrique <7henrique18@gmail.com>
2021-12-26 14:50:52 -0300

    :sparkles: Configure Tab Navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useTheme } from "styled-components";
+import { Platform } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 // screens
 import { Dashboard } from "../screens/Dashboard";
@@ -7,11 +10,51 @@ import { Register } from "../screens/Register";
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export const AppRouter = () => {
+  const theme = useTheme();
+
   return (
-    <Navigator>
-      <Screen name="Listagem" component={Dashboard} />
-      <Screen name="Cadastrar" component={Register} />
-      <Screen name="Resumo" component={Register} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarActiveBackgroundColor: theme.colors.primary,
+        tabBarInactiveBackgroundColor: theme.colors.primary,
+        tabBarLabelPosition: "beside-icon",
+        tabBarStyle: {
+          borderTopWidth: 0,
+          height: 60,
+          paddingVertical: Platform.OS === "ios" ? 20 : 0,
+        },
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 };
